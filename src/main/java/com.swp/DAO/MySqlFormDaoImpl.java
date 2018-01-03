e4c1355a5dc787e048b333cc294593615b01cf76
Aapo Pajunen <aapo.pajunen@gmail.com>
2018-01-03 15:05:22 +0200

    Implemented createForm.

--- src/main/java/com.swp/DAO/MySqlFormDaoImpl.java
+++ src/main/java/com.swp/DAO/MySqlFormDaoImpl.java
@@ -28,7 +28,7 @@ public class MySqlFormDaoImpl implements FormDAO {
 
     //ROW MAPPERS
 
-    public static class FormRowMapper implements RowMapper<Form> {
+    public class FormRowMapper implements RowMapper<Form> {
         @Override
         public Form mapRow(ResultSet resultSet, int i) throws SQLException {
             return new Form(
@@ -38,7 +38,8 @@ public class MySqlFormDaoImpl implements FormDAO {
                     resultSet.getInt("orderNo"),
                     resultSet.getString("title"),
                     resultSet.getDate("dateCreated"),
-                    resultSet.getDate("dateAccepted")
+                    resultSet.getDate("dateAccepted"),
+                    getAnswersByFormId(resultSet.getInt("id"))
             );
         }
     }
@@ -118,7 +119,17 @@ public class MySqlFormDaoImpl implements FormDAO {
 
     @Override
     public void deleteFormById(int formId) {
-        final String sql = "DELETE FROM Forms WHERE id = ?";
+        final String formDeleteSql = "DELETE FROM Forms WHERE id = ?";
+
+        final String answersDeleteSql = "DELETE FROM FieldAnswers WHERE formID = ?";
+
+        jdbcTemplate.update(answersDeleteSql, formId);
+        jdbcTemplate.update(formDeleteSql, formId);
+    }
+
+    @Override
+    public void acceptFormById(int formId) {
+        final String sql = "UPDATE Forms SET dateAccepted = CURRENT_DATE () WHERE ID = ?";
 
         jdbcTemplate.update(sql, formId);
     }
@@ -129,7 +140,7 @@ public class MySqlFormDaoImpl implements FormDAO {
 
         int layoutId = jdbcTemplate.queryForObject(layoutIdSql, Integer.class, formId);
 
-        final String sql = "SELECT * FROM Fields WHERE layoutid =" + layoutId + " ORDER BY ordernumber";
+        final String sql = "SELECT * FROM Fields WHERE layoutid = ? ORDER BY orderNumber";
 
         Collection<Field> fields = jdbcTemplate.query(sql, new FieldRowMapper(), layoutId);
 
@@ -197,10 +208,17 @@ public class MySqlFormDaoImpl implements FormDAO {
     }
 
     @Override
-    public void deleteUser(int id) {
+    public void deleteUserById(int id) {
         final String sql = "DELETE FROM Users WHERE id = ?";
 
         jdbcTemplate.update(sql, id);
+    }
+
+    @Override
+    public void deleteUserByUsername(String username) {
+        final String sql = "DELETE FROM Users WHERE username = ?";
+
+        jdbcTemplate.update(sql, username);
     }
 
     @Override
@@ -242,8 +260,23 @@ public class MySqlFormDaoImpl implements FormDAO {
     }
 
     @Override
-    public void createForm(Form form) {
-        //TODO
+    public void createForm(int id, Form form) {
+        final String formSql = "INSERT INTO Forms (layoutId, userId, orderNo, title, dateCreated) VALUES (?,?,?,?,?)";
+        //TODO fix this somehow!
+        final String orderNoSql = "SELECT MAX(orderNo) FROM Forms WHERE layoutId = ?";
+        final String formIdSql = "SELECT LAST_INSERT_ID()";
+
+        final int orderNo = jdbcTemplate.queryForObject(orderNoSql, Integer.class, form.getLayoutID()) + 1;
+        jdbcTemplate.update(formSql, form.getLayoutID(), id, orderNo, form.getTitle(), form.getDateCreated());
+
+        final int formId = jdbcTemplate.queryForObject(formIdSql, Integer.class);
+
+        form.getAnswers().forEach(
+                (ans) -> {
+                    final String answerSql = "INSERT INTO FieldAnswers (fieldId, formId, answer) VALUES (?,?,?)";
+                    jdbcTemplate.update(answerSql, ans.getFieldID(), formId, ans.getAnswer());
+                }
+        );
     }
 
     @Override
@@ -255,7 +288,7 @@ public class MySqlFormDaoImpl implements FormDAO {
         final Collection<AccessRights> accessRights = this.getUserAccessRights(id);
 
         Set<Integer> ids = new HashSet();
-        accessRights.forEach((ar) -> ids.add(ar.getLayoutId()));
+        accessRights.forEach((ar) -> ids.add(ar.getLayoutID()));
 
         MapSqlParameterSource parameters = new MapSqlParameterSource();
         parameters.addValue("ids", ids);
